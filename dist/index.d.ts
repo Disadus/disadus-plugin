32753e00193c796feef0aa4addbed323870d0ea5
John Li (Tet) <tet@tet.moe>
2022-01-20 01:25:09 -0800

    Build with exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 import { APIWrapper } from "./APIWrapper";
+export { APIWrapper };
 export default APIWrapper;
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.APIWrapper = void 0;
 const APIWrapper_1 = require("./APIWrapper");
+Object.defineProperty(exports, "APIWrapper", { enumerable: true, get: function () { return APIWrapper_1.APIWrapper; } });
 exports.default = APIWrapper_1.APIWrapper;
