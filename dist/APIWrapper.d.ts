e5cb34b7c8ac8b28962ff6143237f9a67ffb003e
John Li (Tet) <tet@tet.moe>
2022-01-20 01:05:11 -0800

    build package

--- dist/APIWrapper.d.ts
+++ dist/APIWrapper.d.ts
@@ -8,14 +8,20 @@ export declare type RawResponse<T> = {
     requestID: string;
     response: RequestResponse<T>;
 };
+export declare type RawRequest<T> = {
+    requestID: string;
+    event: string;
+    request: T;
+};
 export declare class APIWrapper {
     _ready: boolean;
     _parent: MessageEventSource | null;
     requests: Map<string, (data: RawResponse<any>) => void>;
     get readyState(): boolean;
+    static _self: APIWrapper;
+    static get self(): APIWrapper;
     constructor();
     processMessage(event: MessageEvent): void;
-    sendMessage(message: string): void;
     ready(event: MessageEvent): void;
     getRequestId(): string;
     sendRequest(name: string, data: any): Promise<RequestResponse<any>>;
--- dist/APIWrapper.js
+++ dist/APIWrapper.js
@@ -7,9 +7,16 @@ class APIWrapper {
         this._parent = null;
         this.requests = new Map();
         window.addEventListener("message", this.processMessage);
+        window.addEventListener("message", this.ready);
     }
     get readyState() {
         return this._ready;
+    }
+    static get self() {
+        if (!APIWrapper._self) {
+            APIWrapper._self = new APIWrapper();
+        }
+        return APIWrapper._self;
     }
     processMessage(event) {
         const message = event.data;
@@ -21,10 +28,10 @@ class APIWrapper {
             this.requests.delete(message.requestID);
         }
     }
-    sendMessage(message) { }
     ready(event) {
         this._parent = event.source;
-        this._ready = true;
+        this._ready = event.data.success;
+        window.removeEventListener("message", this.ready);
     }
     getRequestId() {
         let requestId = Math.random().toString(36).substring(2);
@@ -35,7 +42,14 @@ class APIWrapper {
     }
     sendRequest(name, data) {
         return new Promise((resolve, reject) => {
+            var _a;
             const requestId = this.getRequestId();
+            const message = {
+                requestID: requestId,
+                event: name,
+                request: data,
+            };
+            (_a = this._parent) === null || _a === void 0 ? void 0 : _a.postMessage(message);
             this.requests.set(requestId, (response) => {
                 resolve(response.response);
             });
