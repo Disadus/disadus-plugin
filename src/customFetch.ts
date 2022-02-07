51c98949462ac8e3f66e145fda271b50444f51cd
John Li (Tet) <tet@tet.moe>
2022-02-07 00:37:18 -0800

    patch 2 for ssr fetch

--- src/customFetch.ts
+++ src/customFetch.ts
@@ -1,10 +1,11 @@
-import type fetch from "node-fetch";
-export { RequestInit } from "node-fetch"; // TODO: add more exports as needed here
-
 // eslint-disable-next-line no-eval
 const fetchPromise: Promise<typeof fetch> = eval('import("node-fetch")').then(
   (mod: { default: typeof fetch }) => mod.default
 );
-export const nFetch: typeof fetch = (...args) =>
-  fetchPromise.then((fetch) => fetch(...args));
+export const nFetch: typeof globalThis.fetch = (...args) => {
+  if (globalThis.fetch) {
+    return globalThis.fetch(...args);
+  }
+  return fetchPromise.then((fetch) => fetch(...args));
+};
 export default nFetch;
