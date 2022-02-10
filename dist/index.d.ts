b17f74068d1ad9778ee2a018e00570be3fc895b9
John Li (Tet) <tet@tet.moe>
2022-02-10 10:14:45 -0800

    update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,6 @@
 import { APIWrapper } from "./APIWrapper";
+import * as DisadusTypes from "./types/DisadusTypes";
 export { APIWrapper };
+export { DisadusTypes };
 export default APIWrapper;
 //# sourceMappingURL=index.d.ts.map
