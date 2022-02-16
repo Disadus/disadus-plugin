c1f4e04c80d7f1e62add6533bb1424fd7e9f0a7e
John Li (Tet) <tet@tet.moe>
2022-02-16 09:16:16 -0800

    removed eval node-fetch

--- src/customFetch.ts
+++ src/customFetch.ts
@@ -1,7 +1,10 @@
 // eslint-disable-next-line no-eval
-const fetchPromise: Promise<typeof fetch> = eval('import("node-fetch")').then(
-  (mod: { default: typeof fetch }) => mod.default
-);
+const fetchPromise: Promise<typeof fetch> = import("node-fetch")
+  .then((mod: any) => mod.default)
+  .catch((er) => {
+    console.error("[APIWrapper]", "Fetch not found", er);
+    throw er;
+  });
 export const nFetch: typeof globalThis.fetch = (...args) => {
   if (globalThis.fetch) {
     return globalThis.fetch(...args);
